fb551b6f6279d02e5a193bfc44aa18346120f840
João Victor Maine <joaovmaine@gmail.com>
2024-07-06 13:36:08 -0300

    adicionar linha

---
+++ python/variaveis_e_tipo_de_dados.py
@@ -0,0 +1,13 @@
+nome = 'João Victor'
+sobrenome = 'Maine'
+idade = 21
+ano_nascimento = 2003
+maior_de_idade = 21 >= 18
+altura_metros = 1.79
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Alture em metros:', altura_metros)
